1d9cde523a7c4b57a68355af97abce808535817e
Mike <turner.mike@gmail.com>
2017-12-18 17:57:49 -0500

    started on header lang file

---
+++ app/resources/lang/en-CA/header.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Landing/Home Page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'user_menu_item_1' => 'Quebec Équipe McDoMD',
+    'user_menu_item_2' => 'FR',
+    'user_menu_item_3' => 'Sign In / Create Account',
+    'user_menu_item_4' => '<div class="fi-magnifying-glass"></div>'
+
+];
