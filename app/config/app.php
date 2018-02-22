1b3f79d001cd9284a723e18f9ae119c91ccc7fe8
Mike <turner.mike@gmail.com>
2018-02-22 12:14:54 -0500

    updated php version in app.dockerfile to 7.1.14-fpm

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel Docker Bootstrap'),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'dev'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://links.test'),
+    'url' => env('APP_URL', 'http://laravel-docker-bootstrap.test/'),
 
     /*
     |--------------------------------------------------------------------------
