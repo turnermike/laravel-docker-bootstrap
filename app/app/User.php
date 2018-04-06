110ce3e05168b211d9dd6498100c553672109f2a
Mike <turner.mike@gmail.com>
2018-04-06 13:58:06 -0400

    added secret to User fillable array

--- app/app/User.php
+++ app/app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'google2fa_secret',
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'google2fa_secret',
     ];
 }
